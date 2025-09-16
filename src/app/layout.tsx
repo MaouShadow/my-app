54caf11394148d2e4e73f9c6b32a01a95e32b66c
Yuto <olalekanokikade@gmail.com>
2025-09-16 19:56:23 +0100

    To push footer to the bottom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,10 +27,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
         <Navbar />
-        {children}
+
+        <main className="flex-grow">
+          {/* Main content */}
+          {children}
+        </main>
+
         <Footer />
       </body>
     </html>
