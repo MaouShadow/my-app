29955020641c7db33aa8dfe1762bccecd7ee1f8f
Yuto <olalekanokikade@gmail.com>
2025-09-18 10:54:08 +0100

    fixed error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { CartProvider } from "@/context/CartContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,7 +34,9 @@ export default function RootLayout({
 
         <main className="flex-grow">
           {/* Main content */}
+          <CartProvider>
           {children}
+          </CartProvider>
         </main>
 
         <Footer />
